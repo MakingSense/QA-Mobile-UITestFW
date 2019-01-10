fd46dbd80f377715f5c103cb4549e640b3c659e8
Celeste Senoseain <csenoseain@makingsense.com>
2019-01-10 18:17:13 -0300

    Add new iOS Locators

--- Todo/Todo.MobileUITest/Views/Todo/TodoViewLocator.cs
+++ Todo/Todo.MobileUITest/Views/Todo/TodoViewLocator.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
 namespace Todo.MobileUITest.Views.Todo
 {
     public class TodoViewLocator
     {
-        public Func<AppQuery, AppQuery> AddButton { get; } =
-          new Func<AppQuery, AppQuery>(c => c.Id("NoResourceEntry-0"));
+        public Func<AppQuery, AppQuery> AddButton { get; set; }
+        public Func<AppQuery, AppQuery> TasksList { get; set; }
+        public Func<AppQuery, AppQuery> TaskName { get; set; }
 
-        public Func<AppQuery, AppQuery> TasksList { get; } =
-          new Func<AppQuery, AppQuery>(c => c.Id("NoResourceEntry-3"));
+
+        public TodoViewLocator CreateInstance(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+
+                AddButton = c => c.Id("NoResourceEntry-0");
+                TasksList = c => c.Id("NoResourceEntry-3");
+                TaskName = c => c.Id("TodoItemName");
+            }
+            else
+            {
+                AddButton = c => c.Id("AddButton");
+                TasksList = c => c.Id("TodoList");
+                TaskName = c => c.Id("TodoItemName");
+            }
+            return this;
+        }
     }
 }
